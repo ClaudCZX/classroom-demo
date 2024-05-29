289320c95a9aba5c4b4b78af04b2cb59531b6e08
ClaudCZX <claudiodelafuented@gmail.com>
2024-05-29 18:13:33 -0400

    add Bento and BentoItem components for stream section

--- src/pages/Dashboard/Stream/index.tsx
+++ src/pages/Dashboard/Stream/index.tsx
@@ -1,5 +1,16 @@
+import Bento from "../../UI/Bento/Bento";
+import BentoItem from "../../UI/Bento/BentoItem";
+
 const Stream = () => {
-  return <h1 className="h-full w-full">Stream</h1>;
+  return (
+    <div className="relative w-full">
+      <h1>Stream</h1>
+      <Bento className="grid-cols-10 md:grid-rows-[2fr,0.5fr] auto-rows-[20rem] overflow-scroll">
+        <BentoItem className="col-span-10 md:col-span-7"></BentoItem>
+        <BentoItem className="col-span-10 md:col-span-3"></BentoItem>
+      </Bento>
+    </div>
+  );
 };
 
 export default Stream;
